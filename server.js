b7c79267d7ff9494d68df5f9530914e32008ece2
Ceryssc <cs.clarke90@gmail.com>
2018-10-26 14:57:43 +1300

    Fixed the redirection bug

--- server.js
+++ server.js
@@ -23,22 +23,16 @@ const homePage = {
 server.get("/", (req, res) => res.render("hotel", homePage));
 
 server.get("/hotel1", (req, res) => res.render("hotel1", homePage));
-
-
 server.post('/hotel1', (req, res) => {
-  
 res.send('Thank you for your review')});
 
-
-server.get("/hotel2", (req, res) => {
-
+server.get("/hotel2", (req, res) => res.render("hotel1", homePage));
+server.post('/hotel2', (req, res) => {
 res.send('Thank you for your review')});
 
-server.get("/hotel3", (req, res) => {
-  
+server.get("/hotel3", (req, res) => res.render("hotel1", homePage));
+server.post('/hotel3', (req, res) => {
 res.send('Thank you for your review')});
-
-
 
 module.exports = server
 
