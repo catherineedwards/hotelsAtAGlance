4b170e5d1d075c0f2a8889a25ade367f1553a53d
catherineedwards <catherine@edwards-au.com>
2018-10-26 13:09:17 +1300

    Ready for Heroku testing

--- routes.js
+++ routes.js
@@ -5,10 +5,11 @@ let router = express.Router();
 // let data = fs.readFile("./data.json");
 
 router.get("/", function(req, res) {
-    res.send("Hello world!")
-})
+  res.send("Hello world!");
+});
 
-
+router.post("/", function(req, res) {
+  console.log(data);
+});
 
 module.exports = router;
-
