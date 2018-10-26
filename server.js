2091a3e2e67157bcda5a77468c70de153750ffa2
catherineedwards <catherine@edwards-au.com>
2018-10-26 14:43:56 +1300

    Update server

--- server.js
+++ server.js
@@ -23,20 +23,17 @@ const homePage = {
 server.get("/", (req, res) => res.render("hotel", homePage));
 
 server.get("/hotel1", (req, res) => res.render("hotel1", homePage));
-
-
 server.post('/hotel1', (req, res) => {
+res.send('Thank you for your review')});
+
+server.get("/hotel2", (req, res) => res.render("hotel2", homePage));
+server.post("/hotel2", (req, res) => {
+res.send('Thank you for your review')});
+
+server.get("/hotel3", (req, res) => res.render("hotel1", homePage));
+server.post('/hotel3', (req, res) => {
+res.send('Thank you for your review')});
   
-res.send('Thank you for your review')});
-
-
-server.get("/hotel2", (req, res) => {
-
-res.send('Thank you for your review')});
-
-server.get("/hotel3", (req, res) => {
-  
-res.send('Thank you for your review')});
 
 
 
